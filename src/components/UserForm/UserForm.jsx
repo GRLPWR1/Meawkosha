fc392047fbb9085ca3aa003778b3706ba434259b
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-06-07 14:35:31 +0300

    created branches

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Form(props) {
+function UserForm(props) {
     return (
         <div>
 
@@ -8,4 +8,4 @@ function Form(props) {
     );
 }
 
-export default Form;
+export default UserForm;
