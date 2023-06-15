5a9edb52cf7a3333813ee61a61c4c4694b0efdc4
katvil <pakhomovacatherine@gmail.com>
2023-06-15 21:32:00 +0300

    add hooks and context

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/UserForm/App";
+import App from "./App";
 import Main from './components/Main/Main'
+import { globalContext as GlobalContext } from "./components/contexts/globalContext";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+
+
+function General() {
+  const initialState = {
+    list: [],
+  };
+
+  const [state, dispatch] = useLocalStorage("list", initialState);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <GlobalContext.Provider value={{ state, dispatch }}>
     <App />
     <Main/>
-  </React.StrictMode>
+  </GlobalContext.Provider>
+    
 );
+}
+export default General;
